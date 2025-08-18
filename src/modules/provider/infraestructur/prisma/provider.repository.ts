f02ff199eb6e368faf764d1adc705398b941b385
MichaelHE-WC <dev3@workcorp.net>
2025-08-18 11:24:30 -0400

    pasos1.5

--- src/modules/provider/infraestructur/prisma/provider.repository.ts
+++ src/modules/provider/infraestructur/prisma/provider.repository.ts
@@ -8,12 +8,8 @@ export class ProviderRepository {
     async createProvider(data:any){
         return this.prisma.provider.create({
             data:{
-                firstName: data.firstName,
-                lastName:data.lastName,
                 NIT: data.NIT ,
-                phone: data.phone,
                 email: data.email,
-                address: data.address,
                 typeProvider: data.typeProvider,
                 status: data.status,
                 isActive: data.isActive,
@@ -23,14 +19,10 @@ export class ProviderRepository {
         })
     }
     async updateProvider(data:any){
-        return await this.prisma.provider.update({where:{id:data.id},
+        return await this.prisma.provider.update({ where:{id:data.id},
             data:{
-              firstName: data.firstName,
-                lastName:data.lastName,
                 NIT: data.NIT ,
-                phone: data.phone,
                 email: data.email,
-                address: data.address,
                 typeProvider: data.typeProvider,
                 status: data.status,
                 isActive: data.isActive,
